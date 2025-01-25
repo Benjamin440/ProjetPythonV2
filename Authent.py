f646ce95f4cf8044e23f04e9978e6fbccc501c0e
Benjamin <aodrenleberre@gmail.com>
2025-01-25 14:36:39 +0100

    Filtre par ville

--- Authent.py
+++ Authent.py
@@ -14,30 +14,12 @@ def authent(login, password):
 
 def verify_role(login):
     res = SqlRequest.select_user(login) 
+    ville = res[0][3]
     if res[0][8] == "UTILISATEUR":
         Crud.menu_utilisateur()
     elif res[0][8] == "ADMIN":
-        Crud.menu_admin()
+        Crud.menu_admin(ville)
     else:
-        Crud.menu_super_admin()
+        Crud.menu_super_admin(ville)
 
-def verify_ville(ville):
-    villes_autorisees = {"PARIS", "RENNES", "STRASBOURG", "GRENOBLE", "NANTES"}
-    if not ville:
-        raise ValueError("La ville ne peut pas être vide")
-    if ville.upper() not in villes_autorisees:
-        raise ValueError("La ville doit être parmi les suivantes : PARIS, RENNES, STRASBOURG, GRENOBLE, NANTES")
-    return ville.upper()
 
-def afficher_user_ville(ville):
-    try:
-        ville = verify_ville(ville)
-        res2 = SqlRequest.select_ville(ville)
-        if not res2:
-            print(f"Aucun utilisateur trouvé pour la ville {ville}.")
-            return
-        print(f"Liste des utilisateurs pour la ville {ville} :")
-        for row in res2:
-            print(row) 
-    except ValueError as e:
-        print(f"Erreur : {e}")
