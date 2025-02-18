de459d0f3413a06074f9526b7899a023d2aefb2e
Benjamin <aodrenleberre@gmail.com>
2025-02-18 08:56:09 +0100

    AjoutT

--- Authent.py
+++ Authent.py
@@ -3,13 +3,17 @@ from User import User
 import Crud
 
 def authent(login, password):
+    cpt = 1
     password = User.hash_password(None, password)
     res = SqlRequest.select_user(login)
     if res and password == res[0][7]:
         print("Vous êtes connecté")
         return True
     else:
-        print("Mauvais nom d'utilisateur ou mot de passe")
+        while (cpt <= 3):
+            print("Mauvais nom d'utilisateur ou mot de passe")
+            print("Il vous reste ", 3-cpt, "tentatives")
+            cpt +=1
         return False
 
 def verify_role(login):
